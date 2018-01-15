852ef3705822b5b2a909fc0784562ad17dfae577
jaredblack <jaredblack@users.noreply.github.com>
2018-01-14 23:10:14 -0800

    Latest version with metronome and 3 valve layout w/ debugging

--- BrassScalePractice/app/src/main/java/com/jhblack/brassscalepractice/MainActivity.java
+++ BrassScalePractice/app/src/main/java/com/jhblack/brassscalepractice/MainActivity.java
@@ -1,53 +1,55 @@
 package com.jhblack.brassscalepractice;
+
 import android.content.Context;
 import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private Context mContext;
     private SoundPool soundPool;
-    private boolean firstTime = true;
-    int soundId;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    int metId;
+    Button valve1;
+    Button valve2;
+    Button valve3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         mContext = getApplicationContext();
         soundPool = new SoundPool.Builder().build();
-        soundId = soundPool.load(mContext, R.raw.a4, 1);
-        Button bt = findViewById(R.id.button);
+        metId = soundPool.load(mContext, R.raw.click, 1);
 
-        bt.setOnTouchListener(new View.OnTouchListener() {
-            int newSoundId = soundId;
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
+        valve1 = findViewById(R.id.valve_1);
+        valve2 = findViewById(R.id.valve_2);
+        valve3 = findViewById(R.id.valve_3);
 
-
-                switch (motionEvent.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        if(firstTime) {
-                            newSoundId = soundPool.play(soundId, 1, 1, 1, -1, 1f);
-                            firstTime = false;
-                        } else {
-                            soundPool.resume(soundId);
-                        }
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        soundPool.pause(newSoundId);
-
-                        Log.i("MotionEvent.ACTION_UP", "original soundId was " + soundId + " newSoundId: " + newSoundId);
-                        break;
-                }
-                return false;
-            }
-        });
+        valve1.setOnTouchListener(new ValveOnTouchListener(1));
+        valve2.setOnTouchListener(new ValveOnTouchListener(2));
+        valve3.setOnTouchListener(new ValveOnTouchListener(3));
     }
 
+    public void startMetronome(View view) {
+        Timer met = new Timer();
+        TimerTask metTask = new TimerTask() {
 
-
+            @Override
+            public void run() {
+                soundPool.play(metId, 1, 1, 1, 0, 1f);
+                boolean[] valves = ValveOnTouchListener.getValvesPressed();
+                if(valves[1] && valves[2] && valves[3]) {
+                    Log.d("MainActivity", "You Are a Good Boi");
+                } else {
+                    Log.d("MainActivity", "Bad Valves!");
+                }
+            }
+        };
+        met.schedule(metTask, 0L, 600L);
+    }
 }
