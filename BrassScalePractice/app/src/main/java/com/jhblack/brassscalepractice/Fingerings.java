6aeb89fa264d4e87eb474740033a4212c8d3405e
jaredblack <jaredblack@users.noreply.github.com>
2018-01-20 14:16:32 -0800

    Added interface with constants defined for each valve combination

---
+++ BrassScalePractice/app/src/main/java/com/jhblack/brassscalepractice/Fingerings.java
@@ -0,0 +1,15 @@
+package com.jhblack.brassscalepractice;
+
+/**
+ * Created by Jared on 1/20/2018.
+ */
+
+public interface Fingerings {
+    boolean[] TTT = {true,true,true};
+    boolean[] TFT = {true,false,true};
+    boolean[] FTT = {false,true,true};
+    boolean[] TTF = {true,true,false};
+    boolean[] TFF = {true,false,false};
+    boolean[] FTF = {false,true,false};
+    boolean[] FFF = {false,false,false};
+}
