b6be538d8e4ab9f163be3cfb4823fadecb9e7563
jaredblack <jaredblack@users.noreply.github.com>
2018-01-24 20:22:03 -0800

    inferior desktop changes

--- BrassScalePractice/app/src/main/java/com/jhblack/brassscalepractice/Scale.java
+++ BrassScalePractice/app/src/main/java/com/jhblack/brassscalepractice/Scale.java
@@ -12,14 +12,18 @@ public class Scale {
     private int currentNote;
     private boolean goingUp = true;
 
-    public Scale(Note startingNote, ScaleType scaleType) {
+    public Scale(Note startingNote, int[] scaleType) {
         currentNote = 0;
-        scaleArr = new boolean[8][3];
+        scaleArr = new boolean[scaleType.length][3];
+        int noteId = startingNote.getValue();
+        for(int r = 0; r < scaleArr.length; r++) {
+            scaleArr[r] = Note.getNoteById(noteId).getValves();
+            noteId += scaleType[r];
+        }
     }
 
     public boolean[] getNextNote() {
         return scaleArr[currentNote];
-
     }
 
     public int getCurrentNote() {
@@ -43,29 +47,10 @@ public class Scale {
     }
 
     public String getNoteName() {
-        switch(currentNote) {
-            case 0: return "C";
-            case 1: return "D";
-            case 2: return "E";
-            case 3: return "F";
-            case 4: return "G";
-            case 5: return "A";
-            case 6: return "B";
-            case 7: return "C";
-        }
-        return "L#";
+        return Note.getNoteById(currentNote).getName();
     }
 
-    public int[] getNoteNames() {
-        int[] noteFiles = new int[8];
-        noteFiles[0] = R.raw.bb3;
-        noteFiles[1] = R.raw.c4;
-        noteFiles[2] = R.raw.d4;
-        noteFiles[3] = R.raw.eb4;
-        noteFiles[4] = R.raw.f4;
-        noteFiles[5] = R.raw.g4;
-        noteFiles[6] = R.raw.a4;
-        noteFiles[7] = R.raw.bb4;
-        return noteFiles;
+    public int[] getNoteRawIds() {
+        return
     }
 }
